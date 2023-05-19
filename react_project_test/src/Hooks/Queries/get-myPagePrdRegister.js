fccc32dd61e0e4fb20b938464ab561f73e136301
taeginote <ab315601@naver.com>
2023-05-19 17:36:08 +0900

    📚study : 리액트 쿼리 정리

--- react_project_test/src/Hooks/Queries/get-myPagePrdRegister.js
+++ react_project_test/src/Hooks/Queries/get-myPagePrdRegister.js
@@ -2,17 +2,17 @@ import { useQuery } from '@tanstack/react-query'
 import QUERY_KEY from '../../Consts/query.key'
 import MyPageApi from '../../Apis/mypageApi'
 
-const getMyPageMainData = async () => {
+const getMyPagePrdRegisterData = async () => {
 	const res = await MyPageApi.productList()
 	return res.data
 }
 
 const useGetMyPagePrdRegisterData = () => {
 	const { data, error, status, isLoading, isError } = useQuery(
-		[QUERY_KEY.GET_MYPAGE_MAIN_DATA],
-		() => getMyPageMainData(),
+		[QUERY_KEY.GET_MYPAGE_REGISTER_DATA],
+		() => getMyPagePrdRegisterData(),
+		{ staleTime: 1000 * 60 * 5 },
 	)
-
 	return { data, error, status, isLoading, isError }
 }
 export default useGetMyPagePrdRegisterData

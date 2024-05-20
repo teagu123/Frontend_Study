50733ed2dd61c60a03fe4207cae1a606d509d311
DevTeddy123 <ab315601@naver.com>
2024-05-20 15:42:53 +0900

    Study : Vanila_ReactRouter 구현

--- react_route_dom_test/src/ReactRouterDom/Pages/page1.js
+++
@@ -1,4 +0,0 @@
-function Page1() {
-	return <h1>🎉Page1 페이지에 오신걸 환영합니🎉</h1>
-}
-export default Page1
--- react_route_dom_test/src/ReactRouterDom/Pages/page3.js
+++
@@ -1,4 +0,0 @@
-function Page3() {
-	return <h1>🎉Page3 페이지에 오신걸 환영합니🎉</h1>
-}
-export default Page3

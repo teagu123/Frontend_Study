d575bd3935d3d16ff216dd8686bc70e1eda398fa
DevTeddy123 <ab315601@naver.com>
2024-10-10 11:56:42 +0900

    feat : 초기 세팅

--- Stomp_Chatting/src/App.tsx
+++ Stomp_Chatting/src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Wrapper>
+			<Box></Box>
+		</Wrapper>
+	)
 }
 
-export default App;
+export default App
+
+const Wrapper = styled.div`
+	width: 100v;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`
+const Box = styled.div`
+	width: 600px;
+	height: 80%;
+	border-radius: 30px;
+	background-color: #e9e5e5;
+`
